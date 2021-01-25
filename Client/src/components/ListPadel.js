3083e59cdc2c5c85818fa4dec2463124214da53a
davide.pizzi <davidepizzi88@gmail.com>
2021-01-25 23:39:28 +0100

    card styles

--- Client/src/components/ListPadel.js
+++ Client/src/components/ListPadel.js
@@ -3,26 +3,30 @@ import Fetch from 'react-fetch-component';
 
 export default class ListPadel extends Component {
   render() {
-
     const url = `/api/padel`;
-    console.log('url', url);
 
     return (
       <div>
-        <h1>Padel</h1>
+        <h1><strong>Padel</strong> <span class="text-muted small">next 15 days</span></h1>
         <Fetch url={url} >
           {({ loading, data, error }) => (
             <div>
-              {loading && <span>Loading...</span>}
-              {console.log("data", data)}
-              <ul>
-                {data && data.map((val, i) => {
-                  return <div key={i}>
-                      <li>{val.date + ' ' + val.duration + ' ' + val.weekday}</li>
-                      <p>{val.name + ' ' + val.address + ' -- (' + val.cost + ')'}</p>
-                    </div>
-                })}
-              </ul>
+              {loading && <div class="spinner-border mt-4" role="status">
+                <span class="visually-hidden"></span>
+              </div>}
+              {data && data.map((val, i) => {
+                return <div class="card text-center mb-3 text-primary">
+                  <div class="card-header">
+                    <span class="badge rounded-pill bg-info">{val.date}</span>
+                    <span class="small text-muted p-3">{val.weekday}</span>
+                  </div>
+                  <div class="card-body">
+                    <h4 class="card-title text-info"><strong>{val.name}</strong></h4>
+                    <p class="card-text">{val.address}</p>
+                  </div>
+                  <div class="card-footer text-muted small p-1">{val.duration + ' -- ' + val.cost}</div>
+                </div>
+              })}
             </div>
           )}
         </Fetch>
